b734683e8a33d2f715df8a1b990468fd8eb2781c
Simon Berner <sibebzh@gmail.com>
2021-06-09 15:37:29 +0200

    init: :tada: initialize the project

---
+++ EmojiArt21/EmojiArt21App.swift
@@ -0,0 +1,17 @@
+//
+//  EmojiArt21App.swift
+//  EmojiArt21
+//
+//  Created by Simon Berner on 09.06.21.
+//
+
+import SwiftUI
+
+@main
+struct EmojiArt21App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
