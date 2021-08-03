6dfafb515d3d63a0db8cfd56abbe0c51c3e236b8
Simon Berner <sibebzh@gmail.com>
2021-08-03 10:49:14 +0200

    feat(PaletteChooser): :construction: add editMode to the PaletteManager
    Add an editMode to the PaletteManager. This puts the list of palettes
    into editMode, so that each palette can be deleted or moved up and down.
    (The icons come with func onDelete and onMove)

--- EmojiArt21/PaletteManager.swift
+++ EmojiArt21/PaletteManager.swift
@@ -5,6 +5,13 @@ import SwiftUI
 struct PaletteManager: View {
     // inject the store (like it is in the PaletteChooser view)
     @EnvironmentObject var store: PaletteStore
+    // Environment variable to change colorScheme, can be set for a certain view
+    // key path as argument to the thing which we are interested in
+    // here the var which has the value from the \.colorScheme
+    // (Documentation: EnvironmentValues)
+    @Environment(\.colorScheme) var colorScheme
+    
+    @State private var editMode: EditMode = .inactive
     
     var body: some View {
         NavigationView {
@@ -14,14 +21,40 @@ struct PaletteManager: View {
                     // NavigationLink view only work in conjunction with a NavigationView!
                     NavigationLink(destination: PaletteEditor(palette: $store.palettes[palette])) {
                         VStack(alignment: .leading, spacing: nil, content: {
+                            // could be added to Text: .font(editMode == .active ? .largeTitle : .caption)
                             Text(palette.name)
                             Text(palette.emojis)
                         })
+                        // if the editMode is active add the tap gesture, otherwise nil
+                        .gesture(editMode == .active ? tap : nil)
                     }
+                }
+                // is the editMode .active, every palette in the List can be deleted...
+                .onDelete { indexSet in
+                    store.palettes.remove(atOffsets: indexSet)
+                }
+                // ...and moved up or down
+                .onMove { indexSet, newOffset in
+                    store.palettes.move(fromOffsets: indexSet, toOffset: newOffset)
                 }
             }
             .navigationTitle("Manage Palettes")
+//            .environment(\.colorScheme, .dark)
             .navigationBarTitleDisplayMode(.inline)
+            .toolbar {
+                // this edit button is looking at the binding $editMode and toggles it
+                // setting the view into edit mode
+                EditButton()
+            }
+            // List is using the binding to check whether it is in edit mode or not
+            .environment(\.editMode, $editMode)
+        }
+    }
+    
+    // for later use...
+    var tap: some Gesture {
+        TapGesture().onEnded {
+            
         }
     }
 }
@@ -39,5 +72,6 @@ struct PaletteManager_Previews: PreviewProvider {
         PaletteManager()
             .previewDevice("iPhone 12 mini")
             .environmentObject(PaletteStore(named: "Preview"))
+            .preferredColorScheme(.light)
     }
 }
